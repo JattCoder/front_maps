0f6c28cbcd91f5442a0d820e03ebd56c57e00b9b
JattCoder <harmandeepmand.hm@gmail.com>
2020-08-07 13:07:27 -0400

    login config

---
+++ JS/Configure/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Login = () => {
+    return(
+        <View>
+            <Text>Lets Config your account and and get all data and save it to state</Text>
+        </View>
+    )
+}
+
+export default Login
