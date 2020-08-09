24984c42c81aa65b92be76606fcdd3ec42e4fd70
Harmandeep Mand <mr.mand@Harmandeeps-MacBook-Air.local>
2020-08-08 23:41:23 -0400

    device info of user

--- JS/Configure/login.js
+++ JS/Configure/login.js
@@ -1,11 +1,23 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import { View, StyleSheet,Text } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { currentuser } from '../../actions/currentuser/currentuser'
+import DeviceInfo from 'react-native-device-info';
+
+// or ES6+ destructured imports
+
+//import { getUniqueId, getManufacturer } from 'react-native-device-info';
 
 const Login = (props) => {
     const dispatch = useDispatch()
 
+    useEffect(()=>{
+        DeviceInfo.getMacAddress().then(mac => {
+            alert(mac)
+        });
+    })
+
+    
     return(
         <View style={Styles.Page}>
             <Text style={{color:'white'}}>Lets Config your account {props.route.params.user.name}</Text>
